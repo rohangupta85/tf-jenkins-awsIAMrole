1ea59cdb41d57d6c45ef644fd71b1240b8baf143
rohangupta85 <37558841+rohangupta85@users.noreply.github.com>
2024-02-27 14:39:44 -0500

    Delete backend.tf

--- backend.tf
+++
@@ -1,8 +0,0 @@
-terraform {
-  backend "s3" {
-    bucket         = "rg-terraform-state-jenkins"
-    key            = "./terraform.tfstate"
-    region         = "us-east-1"
-    dynamodb_table = "rg_ddb_table"
-  }
-}
