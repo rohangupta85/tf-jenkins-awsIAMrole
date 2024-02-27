bacf0cac3928c5f032f4caec799832a84481deee
rohangupta85 <37558841+rohangupta85@users.noreply.github.com>
2024-02-27 14:38:31 -0500

    Update backend.tf

--- backend.tf
+++ backend.tf
@@ -1,6 +1,6 @@
 terraform {
   backend "s3" {
-    bucket         = "rg-terraform-state"
+    bucket         = "rg-terraform-state-jenkins"
     key            = "./terraform.tfstate"
     region         = "us-east-1"
     dynamodb_table = "rg_ddb_table"
